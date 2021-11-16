41085cb647ed39d59ba53ff6fc1f32a9b604ef5f
Thilin98 <thlini1998@gmail.com>
2021-11-16 11:37:22 +0530

    view orders made by a user

---
+++ resources/views/frontend/user/orders.blade.php
@@ -0,0 +1,24 @@
+@extends('frontend.layouts.app')
+
+@section('title', __('My Orders'))
+
+@section('content')
+    <div class="container py-4">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <x-frontend.card>
+                    <x-slot name="header">
+                        @lang('My Orders')
+                    </x-slot>
+
+                    <x-slot name="body">
+                    @foreach($orders as $order)
+                    <div>{{ $order->id}}</div>                   
+                    @endforeach                 
+                    </x-slot>
+
+                </x-frontend.card>
+            </div><!--col-md-10-->
+        </div><!--row--> 
+    </div><!--container-->
+@endsection
